75a88dbed6a6b731f44e61af68be4de326daed8e
Julian Acosta <julacosta9@gmail.com>
2020-05-19 21:17:17 -0700

    add use of routes

--- server.js
+++ server.js
@@ -1,35 +1,29 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-
-const db = require("./client/src/models/index.js");
+const routes = require("./client/src/routes/index.js");
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static assets (usually on heroku)
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 } else {
     app.use(express.static("client/public"));
 }
 
+// API and view routes
+app.use(routes);
+
 // Connect to database
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/feedbackLoft",
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 );
-
-// API routes
-
-// Other requests go to the React app
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
