a98862eea694394a722cd8a9f28e0b28b89a328e
Julian Acosta <julacosta9@gmail.com>
2020-05-22 14:29:48 -0700

    test projects and comments are now generated using actual user and project IDs

--- seed.js
+++ seed.js
@@ -6,6 +6,15 @@ mongoose.connect(
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 );
 
+let projectArr = [];
+let commentArr = [];
+let allUsers = [];
+
+function random(max) { 
+    // This will output a random number between 0 and max
+    return Math.floor(Math.random() * max);
+}
+
 const userSeed = [
     {
         username: "Kennybeats",
@@ -49,136 +58,54 @@ const userSeed = [
     }
 ];
 
-const projectSeed = [
-    {
-        name: "Test Project 1",
-        userId: "1235678910",
-        username: "Kennybeats",
-        genre: "Trap",
-        url: '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/421372218&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
-        description: "would love to get some feedback on the kick/bass levels and if you think the main riff gets to repetitive. Any ideas to keep it interesting?",
-        dateCreated: new Date(Date.now()),
-        lastCommentDate: new Date(Date.now())
-    },
-    {
-        name: "Test Project 2",
-        userId: "1235678910",
-        username: "haywyre",
-        genre: "Hip-Hop",
-        url: '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/445735794&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
-        description: "would love to get some feedback on the kick/bass levels and if you think the main riff gets to repetitive. Any ideas to keep it interesting?",
-        dateCreated: new Date(Date.now()),
-        lastCommentDate: new Date(Date.now())
-    },
-    {
-        name: "Test Project 3",
-        userId: "1235678910",
-        username: "kaytranada",
-        genre: "R&B",
-        url: '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/421372218&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
-        description: "would love to get some feedback on the kick/bass levels and if you think the main riff gets to repetitive. Any ideas to keep it interesting?",
-        dateCreated: new Date(Date.now()),
-        lastCommentDate: new Date(Date.now())
-    },
-    {
-        name: "Test Project 4",
-        userId: "1235678910",
-        username: "monte booker",
-        genre: "Alternative",
-        url: '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/445735794&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
-        description: "would love to get some feedback on the kick/bass levels and if you think the main riff gets to repetitive. Any ideas to keep it interesting?",
-        dateCreated: new Date(Date.now()),
-        lastCommentDate: new Date(Date.now())
-    },
-    {
-        name: "Test Project 5",
-        userId: "1235678910",
-        username: "Jsix",
-        genre: "Dubstep",
-        url: '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/421372218&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
-        description: "would love to get some feedback on the kick/bass levels and if you think the main riff gets to repetitive. Any ideas to keep it interesting?",
-        dateCreated: new Date(Date.now()),
-        lastCommentDate: new Date(Date.now())
-    }
-];
-
-const commentSeed = [
-    {
-        authorId: "1235678910",
-        authorUsername: "Jsix",
-        text: "this song sounds great. I think you need to eq the bass to create more space for the kick. The levels on the percussion are a little all over the place. Use a reference track and try to match those levels, then throw a little compression on it to glue it together. overall, love the concept and think it just needs work on the technical aspects like the mixdown. great job.",
-        rating: 2,
-        projectId: "12345678910",
-        projectName: "Test Project 5",
-        dateCreated: new Date(Date.now()),
-    },
-    {
-        authorId: "1235678910",
-        authorUsername: "monte booker",
-        text: "this song sounds great. I think you need to eq the bass to create more space for the kick. The levels on the percussion are a little all over the place. Use a reference track and try to match those levels, then throw a little compression on it to glue it together. overall, love the concept and think it just needs work on the technical aspects like the mixdown. great job.",
-        rating: 3,
-        projectId: "12345678910",
-        projectName: "Test Project 4",
-        dateCreated: new Date(Date.now()),
-    },
-    {
-        authorId: "1235678910",
-        authorUsername: "kaytranada",
-        text: "this song sounds great. I think you need to eq the bass to create more space for the kick. The levels on the percussion are a little all over the place. Use a reference track and try to match those levels, then throw a little compression on it to glue it together. overall, love the concept and think it just needs work on the technical aspects like the mixdown. great job.",
-        rating: 1,
-        projectId: "12345678910",
-        projectName: "Test Project 3",
-        dateCreated: new Date(Date.now()),
-    },
-    {
-        authorId: "1235678910",
-        authorUsername: "haywyre",
-        text: "this song sounds great. I think you need to eq the bass to create more space for the kick. The levels on the percussion are a little all over the place. Use a reference track and try to match those levels, then throw a little compression on it to glue it together. overall, love the concept and think it just needs work on the technical aspects like the mixdown. great job.",
-        rating: 2,
-        projectId: "12345678910",
-        projectName: "Test Project 2",
-        dateCreated: new Date(Date.now()),
-    },
-    {
-        authorId: "1235678910",
-        authorUsername: "kennybeats",
-        text: "this song sounds great. I think you need to eq the bass to create more space for the kick. The levels on the percussion are a little all over the place. Use a reference track and try to match those levels, then throw a little compression on it to glue it together. overall, love the concept and think it just needs work on the technical aspects like the mixdown. great job.",
-        rating: 3,
-        projectId: "12345678910",
-        projectName: "Test Project 1",
-        dateCreated: new Date(Date.now()),
-    }
-];
-
-db.Project.deleteMany({})
-    .then(() => db.Project.collection.insertMany(projectSeed))
-    .then((data) => {
-        console.log(data.result.n + " records inserted!");
-        process.exit(0);
-    })
-    .catch((err) => {
-        console.error(err);
-        process.exit(1);
-    });
-
+// Generate test users
 db.User.deleteMany({})
-    .then(() => db.User.collection.insertMany(userSeed))
-    .then((data) => {
+    .then(() => db.User.collection.insertMany(userSeed)) 
+    .then(data => {
         console.log(data.result.n + " records inserted!");
-        process.exit(0);
+        return db.User.find({})
     })
-    .catch((err) => {
-        console.error(err);
-        process.exit(1);
-    });
+    .then(data => {
+        // map through test users to create test projects by each user
+        allUsers = data;
+        projectArr = data.map((user, index) => {
+            return {
+                name: `Test Project ${index + 1}`,
+                userId: user._id,
+                username: user.username,
+                genre: "Trap",
+                url: '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/421372218&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
+                description: "would love to get some feedback on the kick/bass levels and if you think the main riff gets to repetitive. Any ideas to keep it interesting?",
+                dateCreated: new Date(Date.now()),
+                lastCommentDate: new Date(Date.now())
+            }
+        })
+        return db.Project.insertMany(projectArr)
+    })
+    .then(data => {
+        return db.Project.find({})
+    })
+    .then(data => {
+        // map through test users to create test comments by each user
+        commentArr = data.map((project, index) => {
+            const randomNum = random(allUsers.length);
+            return {
+                authorId: allUsers[randomNum]._id,
+                authorUsername: allUsers[randomNum].username,
+                text: "this song sounds great. I think you need to eq the bass to create more space for the kick. The levels on the percussion are a little all over the place. Use a reference track and try to match those levels, then throw a little compression on it to glue it together. overall, love the concept and think it just needs work on the technical aspects like the mixdown. great job.",
+                rating: 2,
+                projectId: project._id,
+                projectName: project.name,
+                dateCreated: new Date(Date.now()),
+            }
+        })
 
-db.Comment.deleteMany({})
-    .then(() => db.Comment.collection.insertMany(commentSeed))
-    .then((data) => {
-        console.log(data.result.n + " records inserted!");
-        process.exit(0);
+        return db.Comment.insertMany(commentArr)
     })
-    .catch((err) => {
+    .then(data => {
+        process.exit(0)
+    })
+    .catch(err => {
         console.error(err);
         process.exit(1);
     });
