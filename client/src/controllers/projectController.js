5d22fa26ddd581fb49b0339a30a967d60cd290e8
Julian Acosta <julacosta9@gmail.com>
2020-05-26 14:00:37 -0700

    add route to get all projects by a userId

--- client/src/controllers/projectController.js
+++ client/src/controllers/projectController.js
@@ -7,6 +7,12 @@ module.exports = {
             .then((dbModel) => res.json(dbModel))
             .catch((err) => res.status(422).json(err));
     },
+    findAllByUserId: function (req, res) {
+        db.Project.find({ userId: req.params.id })
+            .sort({ dateCreated: -1 })
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    },
     findById: function (req, res) {
         db.Project.findById(req.params.id)
             .then((dbModel) => res.json(dbModel))
