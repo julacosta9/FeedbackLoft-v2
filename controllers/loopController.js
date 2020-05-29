7669ec9d313a91af4257d2a4963a130aade52cf4
Julian Acosta <julacosta9@gmail.com>
2020-05-28 21:28:28 -0700

    try implementing async await

--- controllers/loopController.js
+++ controllers/loopController.js
@@ -1,40 +1,53 @@
 const db = require("../models/index.js");
 
 // create helper function that returns true/false if a user is eligible
-function isUserEligible(req, res, userId) {
-    db.User.findById(userId)
+function isUserEligible(userId) {
+    return new Promise(() => {
+        db.User.findById(userId)
         .then((user) => {
-            console.log(user.feedbackGiven, user.feedbackReceived)
+            console.log(typeof user.feedbackGiven)
             if (user.feedbackGiven > user.feedbackReceived) {
-                res.end();
                 return true
             } else {
-                res.end();
                 return false
+            }
+        })
+        .catch((err) => res.status(422).json(err));
+    })
+
+    // db.User.findById(userId)
+    //     .then((user) => {
+    //         console.log(typeof user.feedbackGiven)
+    //         if (user.feedbackGiven > user.feedbackReceived) {
+    //             return true
+    //         } else {
+    //             return false
+    //         }
+    //     })
+    //     .catch((err) => res.status(422).json(err));
+}
+
+async function findProjectForReview (req, res) {
+    db.Project.find({})
+        // add .limit(1000) at a later point?
+        .sort({ lastCommentDate: -1 })
+        .then((dbResponse) => {
+            let isEligibleProjectFound = false;
+            // console.log(`isEligibleProjectFound: ${!isEligibleProjectFound}`)
+            if (!isEligibleProjectFound) {
+                dbResponse.forEach((project) => {
+                    let eligible = await isUserEligible(project.userId);
+                    console.log(eligible)
+                    if (eligible === true) {
+                        isEligibleProjectFound = true;
+                        res.json(project);
+                    }
+                });
             }
         })
         .catch((err) => res.status(422).json(err));
 }
 
 module.exports = {
-    findProjectForReview: function (req, res) {
-        db.Project.find({})
-            // add .limit(1000) at a later point?
-            .sort({ lastCommentDate: -1 })
-            .then((dbResponse) => {
-                let isEligibleProjectFound = false;
-                // console.log(`isEligibleProjectFound: ${!isEligibleProjectFound}`)
-                if (!isEligibleProjectFound) {
-                    dbResponse.forEach((project) => {
-                        let eligible = await isUserEligible(req, res, project.userId);
-                        console.log(eligible)
-                        if (eligible === undefined) {
-                            isEligibleProjectFound = true;
-                            res.json(project);
-                        }
-                    });
-                }
-            })
-            .catch((err) => res.status(422).json(err));
-    },
+    findProjectForReview: findProjectForReview
 };
