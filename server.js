f31e21ecd8a1d66983847f0588104c6d25318e92
Julian Acosta <julacosta9@gmail.com>
2020-06-08 23:46:48 -0700

    add MLAB_URI config variable

--- server.js
+++ server.js
@@ -19,12 +19,15 @@ if (process.env.NODE_ENV === "production") {
 // API and view routes
 app.use(routes);
 
-// Connect to database
+// Connect to database  
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/feedbackLoft",
+    process.env.MONGODB_URI || process.env.MLAB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 );
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
+
+heroku_kz6zxhdk
