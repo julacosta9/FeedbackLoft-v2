1c0dfffc23c361dc60d815da5ed7d63fe094a830
Swedish <tarthurf@yahoo.com>
2020-06-08 17:41:07 -0700

    resturctured inbox funcitonality. Comment deletion is still in progress

--- client/src/components/navbar/widgets/Inbox.js
+++ client/src/components/navbar/widgets/Inbox.js
@@ -6,7 +6,7 @@ import UserContext from "../../../utils/UserContext";
 const Inbox = () => {
     const { notifications } = useContext(UserContext);
 
-    console.log(notifications);
+    // console.log(notifications);
 
     const [message, setMessage] = useState([]);
 
@@ -17,51 +17,110 @@ const Inbox = () => {
     const [showMessages, setMessagesState] = useState(false);
 
     useEffect(() => {}, [showMessages]);
-
+    
+    const removeComment = (obj, index, array) => {
+        obj.isRead = true;
+        // array.slice(index, 1);
+        // setMessage(notifications)
+    }
+    
+    useEffect(() => {}, [message])
+    
     return (
-        <div>
+        <React.Fragment>
             <button
                 className="block"
-                onClick={() =>
-                    showMessages === true
-                        ? setMessagesState(false)
-                        : setMessagesState(true)
+                onClick={() => showMessages === true ?
+                    setMessagesState(false)
+                    :
+                    setMessagesState(true)
                 }
             >
-                {message[0] ? (
+                {message[0] && message[0].isRead === false ?
                     <FontAwesomeIcon icon={["fas", "bell"]} className="relative text-white" />
-                ) : (
+                    :
                     <FontAwesomeIcon icon={["far", "bell"]} className="relative text-white" />
-                )}
+                }
             </button>
-            
-            {showMessages === true ? (
+
+            {showMessages === true ?
                 <React.Fragment>
-                <button onClick={() => setMessagesState(false)} className="fixed top-0 right-0 bottom-0 left-0 bg-black opacity-0 h-full w-full cursor-default" />
-                {/* <div className="flex flex-col mt-2 bg-white text-black rounded-lg px-4 py-4 shadow-sm border-gray-400 absolute space-y-10 right-0"> */}
-                <div className="py-1 mt-2 rounded-md bg-white shadow-xs absolute right-0 w-64" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                    {message[0] ? (
-                        message.map((notification) => {
-                            return (
-                                
-                                        <Notification
-                                            key={notification._id}
-                                            id={notification._id}
-                                            message={notification.message}
-                                            date={notification.dateCreated}
-                                        />
-                            );
+                <div className="py-1 mt-2 rounded-md bg-white shadow-xs absolute right-0 w-64" 
+                    role="menu" 
+                    aria-orientation="vertical" 
+                    aria-labelledby="user-menu"
+                >
+                    {message[0] && message.length > 0 ? 
+                        message.map((notification, i) => {
+                            if (notification.isRead === false) return (
+                                <Notification
+                                    id={notification._id}
+                                    key={notification._id}
+                                    message={notification.message}
+                                    date={notification.dateCreated}
+                                />
+
+                            )
                         })
-                    ) : (
-                        <p>No current notifications</p>
-                    )}
+                        :
+                        (
+                            <div className="text-center">
+                            No Current Notifications
+                            </div>
+                        )
+                    }
                 </div>
                 </React.Fragment>
-            ) : (
-                <div></div>
-            )}
-        </div>
-    );
+                :
+                <div className="invisible"></div>
+            }
+        </React.Fragment>
+    )
+    // return (
+    //     <div>
+    //         <button
+    //             className="block"
+    //             onClick={() => showMessages === true ? 
+    //                 setMessagesState(false)
+    //                 : 
+    //                 setMessagesState(true)
+    //             }
+    //         >
+    //             {message[0] && message[0].isRead === false ? 
+    //                 <FontAwesomeIcon icon={["fas", "bell"]} className="relative text-white" />
+    //                 : 
+    //                 <FontAwesomeIcon icon={["far", "bell"]} className="relative text-white" />
+    //             }
+    //         </button>
+            
+    //         {showMessages === true ? (
+    //             <React.Fragment>
+    //         <button className="fixed top-0 right-0 bottom-0 left-0 bg-black opacity-0 h-full w-full cursor-default" />
+    //             {/* <div className="flex flex-col mt-2 bg-white text-black rounded-lg px-4 py-4 shadow-sm border-gray-400 absolute space-y-10 right-0"> */}
+    //             <div className="py-1 mt-2 rounded-md bg-white shadow-xs absolute right-0 w-64" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+    //                 {message[0] ? (
+    //                     message.map((notification, i) => {
+    //                         if (notification.isRead === false)
+    //                         return (
+    //                             <Notification
+    //                                 id={notification._id}
+    //                                 key={notification._id}
+    //                                 message={notification.message}
+    //                                 date={notification.dateCreated}
+    //                                 /*callback={removeComment(notification, i, notifications)}*/
+    //                             />
+    //                         );
+    //                     })
+    //                 ) : (
+    //                     <p>No current notifications</p>
+    //                 )}
+    //             </div>
+    //             </React.Fragment>
+    //         ) : (
+    //             <div></div>
+    //         )}
+    //     </div>
+    // );
 };
 
 export default Inbox;
